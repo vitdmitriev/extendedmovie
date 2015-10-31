f3b5e798c5ff831c6a9b058739bc805a4ab6e11f
Sovent <vmargelov@gmail.com>
2015-10-31 16:12:41 +0300

    Large file deleted

---
+++ OriginalTrack/Controllers/WebClient/CinemaController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+using OriginalTrack.Models;
+
+namespace OriginalTrack.Controllers.WebClient
+{
+    public class CinemaController : ApiController
+    {
+        // GET: api/Cinema
+        public IEnumerable<Cinema> Get()
+        {
+            return null;
+        }
+
+        // GET: api/Cinema/5
+        public Cinema Get(Guid id)
+        {
+            return null;
+        }
+
+        // POST: api/Cinema
+        public void Post([FromBody]Cinema value)
+        {
+        }
+
+        // DELETE: api/Cinema/5
+        public void Delete(Guid id)
+        {
+        }
+    }
+}
